4cae3b5f170f0766a0cbdc3706e290520e191a41
mludvik2 <mludvik2@yahoo.com>
2025-10-17 12:43:10 +0200

    added introduction code

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ second project for Engeto Online Python Academy
 Author: Michaela Papadimitriu Ludvikova
 email: mludvik2@yahoo.com
 """
+
+print("Hi there!")
+print("-" * 40)
+print("I've generated a random 4 digit number for you.")
+print("Let's play a bulls and cows game.")
+print("-" * 40)
+choice = input("Enter a number: ")
