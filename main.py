eb84d0289cadffcb664af599981002eae534048c
mludvik2 <mludvik2@yahoo.com>
2025-10-17 14:08:50 +0200

    adjusting conditions for looping

--- main.py
+++ main.py
@@ -15,21 +15,35 @@ print("-" * 40)
 rand_num = random.randint(1000, 9999)
 print(rand_num)
 
-choice = int(input("Enter a number: "))
+
 print("-" * 40)
 
 incorrect_num = []
-if not 1000 < choice < 9999:
-    print("You have not chosen a 4 digit number.")
-    print("Try again.")
-    incorrect_num += choice
+while True:
+    choice = int(input("Enter a number: "))
+
+    if choice == rand_num:
+        print(f"Correct, you've guessed the right number in {sum(incorrect_num)} guesses!")
+        break
+    elif not 1000 <= choice <= 9999:
+        incorrect_num.append(choice)
+        print("You have not chosen a 4 digit number.")
+        print("Try again.")
+    elif 1000 <= choice <= 9999:
+        choice != rand_num
+        incorrect_num.append(choice)
+        print("You have not guessed the number!")
+        print("Try again.")
     print(incorrect_num)
-elif choice == rand_num:
-    print(f"Correct, you've guessed the right number in {sum(incorrect_num)} guesses!")
-else:
-    print("Try again.")
-    incorrect_num += choice
-    print(incorrect_num)
+
+
+
+#elif choice == rand_num:
+    #print(f"Correct, you've guessed the right number in {sum(incorrect_num)} guesses!")
+#else:
+    #print("Try again.")
+    #incorrect_num += choice
+    #print(incorrect_num)
 
 #else:
 
