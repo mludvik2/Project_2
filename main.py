5ee30dbabdd4bd88060925946f350b26616c6559
mludvik2 <mludvik2@yahoo.com>
2025-10-17 13:47:18 +0200

    added conditions for 4 digit numbers, if correct number chosen

--- main.py
+++ main.py
@@ -4,10 +4,39 @@ second project for Engeto Online Python Academy
 Author: Michaela Papadimitriu Ludvikova
 email: mludvik2@yahoo.com
 """
+import random
 
 print("Hi there!")
 print("-" * 40)
 print("I've generated a random 4 digit number for you.")
 print("Let's play a bulls and cows game.")
 print("-" * 40)
-choice = input("Enter a number: ")
+
+rand_num = random.randint(1000, 9999)
+print(rand_num)
+
+choice = int(input("Enter a number: "))
+print("-" * 40)
+
+incorrect_num = []
+if not 1000 < choice < 9999:
+    print("You have not chosen a 4 digit number.")
+    print("Try again.")
+    incorrect_num += choice
+    print(incorrect_num)
+elif choice == rand_num:
+    print(f"Correct, you've guessed the right number in {sum(incorrect_num)} guesses!")
+else:
+    print("Try again.")
+    incorrect_num += choice
+    print(incorrect_num)
+
+#else:
+
+    #print("Try again.")
+    #loop through
+
+#def generate_number(length, ):
+
+
+
