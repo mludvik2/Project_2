a2bd4f8a996d26ae986bab69fefb93b89da97b83
mludvik2 <mludvik2@yahoo.com>
2025-10-22 13:44:09 +0200

    addition of play again option and amount of tries

--- main.py
+++ main.py
@@ -95,14 +95,34 @@ def play_game():
                 print(f"Correct, you've guessed the right number in {tries} guesses!")
             print("-" * 60)
             print("That's amazing!")
-            break
+            print("-" * 60)
+            return tries
         
         bulls, cows = count_bulls_and_cows(secret, guess)
         print(f"{bulls} bull{'s' if bulls != 1 else ''}, {cows} cow{'s' if cows != 1 else ''}")
         print(f"Number of guesses: {tries}")
         print("-" * 60)
 
-play_game()
+def main():
+    """Let the Player replay and see their stats."""
+    all_tries = []
+
+    while True:
+        tries = play_game()
+        all_tries.append(tries)
+
+        play_again = input("Would you like to play again? (yes/no): ").strip().lower()
+        if play_again not in ("yes", "y"):
+            print("-" * 60)
+            print("Thank you for playing! Here are your results: ")
+            print(f"Games played: {len(all_tries)}")
+            print("Goodbye!")
+            print("-" * 60)
+            break
+
+main()
+
+
 
 #remove comment and print of secret number
 
